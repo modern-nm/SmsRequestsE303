cc32b1c7a0b128d07621019268ceabbe39c4d5c2
Denis Finkevich <modern-nm@yandex.by>
2022-08-07 22:27:07 +0300

    it works

--- SmsRequestsE303/Program.cs
+++ SmsRequestsE303/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -8,14 +9,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
+
+
+
 namespace SmsRequestsE303
 {
     internal class Program
     {
         static async Task Main(string[] args)
         {
+            System.Net.ServicePointManager.Expect100Continue = false;
+
             HttpClient client = new HttpClient();
-            Uri uri = new Uri("http://192.168.1.1/api/send_sms");
+            client.BaseAddress = new Uri(@"http://192.168.1.1");
+            //Uri uri = new Uri("http://192.168.1.5/awtadfg");
             try
             {
                 string text = args[1];
@@ -23,32 +30,73 @@ namespace SmsRequestsE303
                 string now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"); //2022-07-01 17:52:37
                 string xml = $@"<?xml version=""1.0"" encoding=""UTF-8""?>
                             <request>
-                            <Index>-1</Index >
-                            <Phones><Phone>{phone}</Phone></Phones>
-                            <Sca></Sca>
-                            <Content>{text}</Content>
-                            <Length>{text.Length}</Length >
-                            Reserved>1</ Reserved >
-                            <Date>{now}</Date> 
-                        </request>";
+<Index>-1</Index>
+<Phones><Phone>{phone}</Phone></Phones>
+<Sca></Sca>
+<Content>{text}</Content>
+<Length>{text.Length}</Length>
+<Reserved>1</Reserved>
+<Date>{now}</Date>
+</request>";
+                //                string xm = $@"<?xml version=""1.0"" encoding =""UTF-8""?>
+                //<request>
+                //    <Index>-1</Index>
+                //    <Phones><Phone>+375336742726</Phone></Phones>
+                //    <Sca></Sca>
+                //    <Content>Uasd</Content>
+                //    <Length>4</Length>
+                //    <Reserved>1</Reserved>
+                //    <Date>2022-08-07 17:52:37</Date>
+                //</request>";
+                string xm = $@"<request>
+    <Index>-1</Index>
+    <Phones><Phone>+375336742726</Phone></Phones>
+    <Sca></Sca>
+    <Content>Uasd</Content>
+    <Length>4</Length>
+    <Reserved>1</Reserved>
+    <Date>2022-08-07 17:52:37</Date>
+</request>";
 
-                SmsContent content = new SmsContent(xml, "text/xml");
-                HttpResponseMessage response = await client.PostAsync(uri, content);
-                if(response.StatusCode == HttpStatusCode.OK)
+
+                HttpRequestMessage request = new HttpRequestMessage();
+                request.RequestUri = new Uri(@"http://192.168.1.1/api/sms/send-sms");
+                request.Method = HttpMethod.Post;
+
+
+                request.Content = new StringContent(xm, Encoding.UTF8, "text/xml");
+                //SmsContent content = new SmsContent(xml, "text/xml");
+                //HttpResponseMessage response = await client.PostAsync(uri, content);
+                try
                 {
-                    Console.WriteLine(response.StatusCode);
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    Console.WriteLine(response.RequestMessage);
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Console.WriteLine(response.StatusCode);
+                    }
+                    else
+                    {
+                        Console.WriteLine(response.StatusCode + " smthing went wrong");
+                    }
                 }
-                else
+                catch (Exception f)
                 {
-                    Console.WriteLine(response.StatusCode + " smthing went wrong");
+
+                    Console.WriteLine($"{f.Message}\n{f.StackTrace}\n{f.Source}\n{f.Data}");
                 }
+
+
+
+
+
             }
             catch (Exception e)
             {
 
                 Console.WriteLine($"{e.Message}\n{e.StackTrace}\n{e.Source}\n{e.Data}");
             }
-            
+            Console.ReadLine();
         }
     }
 
@@ -59,12 +107,16 @@ namespace SmsRequestsE303
 
         protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
         {
-            throw new NotImplementedException();
+            Contract.Assert(stream != null);
+
+            return Task.CompletedTask;
         }
 
         protected override bool TryComputeLength(out long length)
         {
-            throw new NotImplementedException();
+            length = Content.Length;
+            return true;
+            
         }
         public SmsContent(string content, string contentType)
         {
